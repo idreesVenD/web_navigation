69878419e6f6b508470f76ef2b2ae9e21299bad7
Muhammad Idrees <idrees.bughio@venturedive.com>
2021-12-22 14:41:47 +0500

    ui changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:url_strategy/url_strategy.dart';
 
@@ -36,32 +38,63 @@ class MyApp extends StatelessWidget {
             settings: settings,
             builder: (context) {
               final PageController controller = PageController();
+              const backgroundGradient = BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xff322043),
+                    Color(0xff1F0C3F),
+                  ],
+                  stops: [0.0, 1.0],
+                  begin: FractionalOffset.topCenter,
+                  end: FractionalOffset.bottomCenter,
+                  tileMode: TileMode.repeated,
+                ),
+              );
               return Scaffold(
                 body: PageView(
                   controller: controller,
                   scrollDirection: Axis.vertical,
                   children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                        gradient: RadialGradient(
-                          center: Alignment(0, 0),
-                          colors: [
-                            Color(0xff2A2068),
-                            Color(0xff110C31).withOpacity(.97),
-                          ],
-                          radius: 0.8,
+                    Stack(
+                      children: [
+                        Container(
+                          decoration: backgroundGradient,
                         ),
-                      ),
-                      child: const Center(
-                        child: Text('First Page'),
-                      ),
+                        ShaderMask(
+                          shaderCallback: (rect) {
+                            return const LinearGradient(
+                              begin: Alignment.topRight,
+                              end: Alignment.bottomLeft,
+                              stops: [0, 0.6],
+                              colors: [Colors.black, Colors.transparent],
+                            ).createShader(
+                                Rect.fromLTRB(0, 0, rect.width, rect.height));
+                          },
+                          blendMode: BlendMode.dstIn,
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.8,
+                              height: MediaQuery.of(context).size.height,
+                              child: Image.network(
+                                "https://image.tmdb.org/t/p/w1280/VlHt27nCqOuTnuX6bku8QZapzO.jpg",
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const TitleSubtitle(),
+                      ],
                     ),
-                    ListView.builder(
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text(index.toString()),
-                        );
-                      },
+                    Container(
+                      color: const Color(0xff1F0C3F),
+                      child: ListView.builder(
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(index.toString()),
+                          );
+                        },
+                      ),
                     ),
                   ],
                 ),
@@ -70,6 +103,51 @@ class MyApp extends StatelessWidget {
           );
         }
       },
+    );
+  }
+}
+
+class TitleSubtitle extends StatelessWidget {
+  const TitleSubtitle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Padding(
+      padding: const EdgeInsets.all(64.0),
+      child: Align(
+        alignment: Alignment.bottomLeft,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Spider-Man:\nNo Way Home',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w900,
+                fontSize: 5 * SizeConfig.blockSizeVertical!,
+              ),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            Text(
+              'Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero.',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                fontSize: 2 * SizeConfig.blockSizeVertical!,
+              ),
+            ),
+            // SizedBox(
+            //   height: 300.0,
+            // ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -129,5 +207,21 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ),
     );
+  }
+}
+
+class SizeConfig {
+  static MediaQueryData? _mediaQueryData;
+  static double? screenWidth;
+  static double? screenHeight;
+  static double? blockSizeHorizontal;
+  static double? blockSizeVertical;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData!.size.width;
+    screenHeight = _mediaQueryData!.size.height;
+    blockSizeHorizontal = screenWidth! / 100;
+    blockSizeVertical = screenHeight! / 100;
   }
 }
